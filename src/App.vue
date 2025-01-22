a62591236925c76d4a95fa4a10d4bec2cd184174
duridev <duridev@outlook.com>
2025-01-21 23:57:43 -0300

    Creando el header

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,15 @@ import { RouterView } from 'vue-router';
 
 
 <template>
+    <header class="bg-slate-800">
+        <div class="mx-auto max-w-6x py-10">
+            <h1 class="text-4xl font-extrabold text-white">Administrador de Clientes</h1>
+        </div>
+    </header>
+    
+    <main class="max-w-6xl mx-auto mt-10">
+    
+    </main>
     <RouterView/>
 
 </template>
