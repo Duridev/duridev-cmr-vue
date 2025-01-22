f59db51d04e2128e8e9dc23b25342c82b2dd5247
duridev <duridev@outlook.com>
2025-01-21 23:46:42 -0300

    Creando la primera vista ClientesView

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Inicio from '@/views/ClientesView.vue'
 
 
 const router = createRouter({
@@ -7,7 +8,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: ,
+      component: Inicio,
     },
   ],
 })
