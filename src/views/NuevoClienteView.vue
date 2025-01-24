735d793a01981be0c05ff4a3f9ccef6dea74bb24
duridev <duridev@outlook.com>
2025-01-24 00:28:39 -0300

    Mostrando el Nombre de la Pagina actual

--- src/views/NuevoClienteView.vue
+++ src/views/NuevoClienteView.vue
@@ -1,15 +1,16 @@
 <script setup>
     import RouterLink from '../components/UI/RouterLink.vue'
+    import Heading from '@/components/UI/Heading.vue';
 </script>
 
 
 <template>
     <div>
-        <div>
+        <div class="flex justify-end">
             <RouterLink to="inicio">
                 Volver
             </RouterLink>
         </div>
-        <h1 class="text-4xl font-extrabold text-slate-500">Agregar Clientes</h1>
+        <Heading>Agregar Clientes</Heading>
     </div>
 </template>
