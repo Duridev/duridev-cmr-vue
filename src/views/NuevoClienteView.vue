3f9ba45acadfedf62c0d8732c45ba89987991350
duridev <duridev@outlook.com>
2025-01-25 23:24:21 -0300

    Añadiendo un Tema para el CSS de Formkit

--- src/views/NuevoClienteView.vue
+++ src/views/NuevoClienteView.vue
@@ -21,11 +21,28 @@
         <Heading>{{ titulo }}</Heading>
 
         <div class="mx-auto mt-10 bg-white shadow">
-            <FormKit
-                type="form"
-            >
-            
-            </FormKit>
+            <div class="mx-auto md:w-2/3 py-20 px-6">
+                <FormKit
+                    type="form"
+                >
+                    <FormKit
+                        type="text"
+                        label="Nombre"
+                        placeholder="Nombre del Cliente"
+                        prefix-icon=""
+                        validation="required"
+                        :validation-messages="{ required: 'El nombre del cliente es obligatorio'}"
+                        validation-visibility="blur"
+                    />
+                </FormKit>
+            </div>
         </div>
     </div>
 </template>
+
+<style scoped>
+    .formkit-wrapper {
+        max-width: 100%;
+    }
+
+</style>
